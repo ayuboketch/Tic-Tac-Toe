f99c87678d2d57bf8b4555575544e82e32c0ffb9
ayub oketch <ayuboketch84@gmail.com>
2024-07-25 14:38:14 +0300

    webpack intergration

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+let path = require("path");
+let HtmlWebpackPlugin = require("html-webpack-plugin");
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let TerserPlugin = require("terser-webpack-plugin");
+let CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+let CopyPlugin = require("copy-webpack-plugin");
+let { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
+};
